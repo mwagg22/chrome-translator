6d7b1de074dd7cb2cd11a1e45c224c4eec943b84
mwagg22 <47122152+mwagg22@users.noreply.github.com>
2020-07-28 12:50:34 -0400

    Add files via upload

--- scr/popup.js
+++ scr/popup.js
@@ -1,5 +1,43 @@
-var filename=((localStorage.getItem("vocabFile") === null))?"":localStorage.getItem("vocabFile")
-var dictionary=((localStorage.getItem("dictionary") === null))?"":localStorage.getItem("dictionary")
+var madea={
+"hi":"hellur",
+"hello":"hellur",
+"boy":"boii",
+"anger":"angression",
+"you":"yo azz",
+"doing":"during",
+"police":"po po",
+"with":"wit",
+"the":"da",
+"morning":"mornTing",
+"lord":"lort(Praise him)",
+"your":"ur",
+"doing":"during",
+"isn't":"ain't",
+"halleluya":"hallelujer",
+"lunch":"lernch",
+"how":"how",
+"friend":"homie",
+"friends":"homies",
+"for":"fo",
+"go":"gow",
+"in":"n",
+"let":"le",
+"me":"meh",
+"explain":"splain",
+"help":"hep",
+"hair":"hare",
+"i've":"i'don",
+"that":"dat",
+"um":"urrrm",
+"juice":"jurrrrce",
+"alright":"urrright",
+"ham":"hurrrm",
+"worry":"wurrry",
+"politically":"politicle",
+"everything":"errything"
+}
+var filename=((localStorage.getItem("vocabFile") === null))?"madea.json":localStorage.getItem("vocabFile")
+var dictionary=((localStorage.getItem("dictionary") === null))?JSON.stringify(madea):localStorage.getItem("dictionary")
 document.addEventListener('DOMContentLoaded', function () {
 document.querySelector('input[type="file"]').addEventListener('change',function(){
 	filename=this.files[0];
@@ -26,8 +64,10 @@ document.querySelector('#translate').addEventListener('click',onclick,false)
 function openFile(){
 var reader = new FileReader();
 reader.readAsText(filename)
+
 reader.onload = function() {
 	dictionary=reader.result
+	console.log(dictionary);
 	localStorage.setItem("dictionary", dictionary);
   };
 
